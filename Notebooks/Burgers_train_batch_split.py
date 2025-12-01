a871f2fb922c90b9591837e8d83464193225fbc3
Jakob Tanderup <s184021@student.dtu.dk>
2025-12-01 12:18:30 +0100

    saturday work

--- Notebooks/Burgers_train_batch_split.py
+++ Notebooks/Burgers_train_batch_split.py
@@ -7,8 +7,8 @@ from pyshred import DataManager, SHRED, SHREDEngine, MLP, PI_SHRED, LSTM, LSTM_P
 from torch.utils.data import DataLoader, random_split
 import Master_utilz as utilz
 from scipy.fft import fft, ifft, fftshift, fftfreq
-from scipy.integrate import solve_ivp
-from torch.autograd import grad
+from io import BytesIO
+from PIL import Image
 
 plt.rcParams.update({
     "font.family": "serif",
@@ -17,8 +17,21 @@ plt.rcParams.update({
     "axes.unicode_minus": False,    # So minus sign appears correctly
 })
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+folder_path = "Burgers_viz/"
+
+lags = 15
+nr_sensors = 3
+nr_t_steps = 200
+model_dtype = torch.float32
+batch_size = 128
+
+make_gif = True
+#Training skips for modified SHRED
+training_skips = 50
+
 
 #Creating data by solving the burgers equation in 1D
 def init_KS(x,t=0):
@@ -58,44 +71,36 @@ def runge_kutta(u0,dudt,dt,iters, Omega,nu):
     print(f"Finished RK4 with {iters} iterations final time = {dt*iters}")
     return u_sol_all,t_all
 
-
 delta_t = 2*np.abs(6 / ((N) ** 2))
-t_end =  1.6037/np.pi*3
+t_end =  1.6037/np.pi*2.5
 
 iters = int(t_end/delta_t)
 img_test = max(u0)*2/N
 u_sol,t = runge_kutta(u0,burgers,delta_t,iters, Omega,nu)
 
-t_skips = iters//200
+t_skips = iters//nr_t_steps
 x_skips = 1
 
 t = t[::t_skips]
 u_sol = u_sol[::t_skips,::x_skips]
-x = x[::x_skips]
+x_coords = x[::x_skips]
 delta_t_synth = delta_t*t_skips
 
-lags = 10
-nr_sensors = 3
 np.random.seed(41)
 sensors  = np.random.choice(np.arange(0,u_sol.shape[1]),nr_sensors,replace = False)
-# test_frames = 5
 
 X_train,y_train = utilz.trajectory_gen([u_sol],lags =lags, sensors=sensors)
-# X_train,y_train,_ = utilz.data_prepare([u_sol],lags =lags, sensors=sensors)
 
-training_skips = 1
-X_train = X_train
-y_train = y_train[:,::training_skips]
-
-model_dtype = torch.float32
+# training_skips = 1
+# X_train = X_train
+# y_train = y_train[:,::training_skips]
 
 Burger_data = {
     'X':X_train,
-    "coords" : x[::training_skips,None],
+    "coords" : x_coords[:,None],
     'y':y_train,
 }
 
-batch_size = 32
 
 Burger_dataset = utilz.SHREDdata_PI(Burger_data)
 train_dataset, val_dataset, _ = Burger_dataset.split_data(train_ratio = 0.6,val_ratio=0.4, test_ratio=0.0 , sequential_split=True)
@@ -117,7 +122,6 @@ torch.set_default_dtype(model_dtype)
 sequence_model = LSTM_PI(hidden_size=32, num_layers=2)
 trunk_model = MLP_TRUNK(hidden_sizes=[64,128], dropout=0.1)
 
-
 jakob_shred = PI_SHRED_V2(sequence_model=sequence_model,
     trunk_model=trunk_model)
 
@@ -129,10 +133,8 @@ criterion = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(jakob_shred.parameters(), lr=0.001)
 
 jakob_shred.train()
-training_skips = 20
-# def get_dx(model, coords):
-#     return model.forward()
-for epoch in range(2000):    
+
+for epoch in range(3500):    
     epoch_loss = 0.0
     for X,coords, y in train_loader:
         coords = coords[0][::training_skips,:]  
@@ -149,7 +151,7 @@ for epoch in range(2000):
         print(f"at epoch {epoch} the data loss is {torch.sum(data_loss):.7f}")
 
 sequence_model = LSTM(hidden_size=32, num_layers=2)
-decoder_model = MLP(hidden_sizes=[350,400], dropout=0.1)
+decoder_model = MLP(hidden_sizes=[64,128], dropout=0.1)
 
 
 SD_shred = PI_SHRED(sequence_model=sequence_model,
@@ -163,7 +165,7 @@ criterion = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(SD_shred.parameters(), lr=0.001)
 
 SD_shred.train()
-for epoch in range(1000):    
+for epoch in range(3000):    
     epoch_loss = 0.0
     for X,coords, y in train_loader:
         coords = coords[0]  
@@ -194,7 +196,9 @@ for X, coords, y in visualize_loader:
         plt.plot(coords.cpu().numpy(), y[0].cpu().numpy(), '--', label='True', linewidth=2)
         if training_skips >= 20:
             plt.plot(coords[::training_skips,:] .cpu().numpy(), y[0,::training_skips].cpu().numpy(), 'o', color = "black",label='Training data used', markersize=2)
-        plt.title('SHRED Predictions')
+        plt.title(f'SHRED Predictions \n'
+                  f"Using {nr_sensors} sensors, lags = {lags}, Nx = {N} and Nt = {nr_t_steps}\n" 
+                  f"Modified model uses {(N//training_skips)/N * 100}% of points in x")
         plt.xlabel('x')
         plt.ylabel('u(x,t)')
         first_sensor = True
@@ -209,3 +213,103 @@ for X, coords, y in visualize_loader:
         plt.show()
         break   
     counter+= 1
+
+if make_gif: 
+
+    images_train = []
+    visualize_loader_train = DataLoader(train_dataset, batch_size=1, shuffle=False)
+
+    jakob_shred.eval()
+    SD_shred.eval()
+    for i, (X, coords, y) in enumerate(visualize_loader_train):
+        print(f"starting loop {i}")
+        coords = coords[0]
+        output_t_jakob, _, _ = jakob_shred.forward(X, coords)
+        output_t_SD = SD_shred.forward(X)
+        plt.figure(figsize=(10,6))
+        plt.plot(coords.cpu().numpy(), output_t_SD[0].detach().cpu().numpy(), label='SHRED', linewidth=2.5)
+        plt.plot(coords.cpu().numpy(), output_t_jakob[0].detach().cpu().numpy(), label='Modified SHRED', linewidth=2.5)
+        plt.plot(coords.cpu().numpy(), y[0].cpu().numpy(), '--', label='True', linewidth=2)
+        if training_skips >= 10:
+                plt.plot(coords[::training_skips,:] .cpu().numpy(), y[0,::training_skips].cpu().numpy(), 'o', color = "black",label='Training data used', markersize=2)
+        # plt.plot(coords[240:250,:] .cpu().numpy(), y[0,240:250].cpu().numpy(), 'o', color = "black",label='Training data used', markersize=2)
+        plt.title(f'SHRED Predictions on train data at t = {t[i]:.4f} \n'
+                  f"Using {nr_sensors} sensors, lags = {lags}, Nx = {N} and Nt = {nr_t_steps}\n" 
+                  f"Modified model uses {(N//training_skips)/N * 100}% of points in x")
+        plt.xlabel('x')
+        plt.ylabel('u(x,t)')
+        first_sensor = True
+        for sensor in sensors:
+            if first_sensor:
+                plt.plot(coords[sensor].cpu().numpy(), y[0,sensor].cpu().numpy(),"o", color='red', label='Sensors',markersize=5)
+                first_sensor = False
+            else:
+                plt.plot(coords[sensor].cpu().numpy(), y[0,sensor].cpu().numpy(),"o", color='red', markersize=5)
+        plt.legend()
+        plt.grid()
+        plt.xlim([-L/2, L/2])
+        plt.ylim([-1.2, 1.2])
+        buf = BytesIO()
+        plt.savefig(buf, format = 'png')
+        buf.seek(0)
+        images_train.append(Image.open(buf))
+        plt.close()
+        plt.show()
+
+    save_path = folder_path + f"Train_s{nr_sensors}_xskips{training_skips}_endtime{t_end:.2f}.gif"
+
+    images_train[0].save(save_path,
+                save_all=True,
+                append_images=images_train[1:],
+                duration=200,
+                loop=0)
+
+    images_test = []
+    visualize_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
+
+    jakob_shred.eval()
+    SD_shred.eval()
+    for i, (X, coords, y) in enumerate(visualize_loader):
+        print(f"starting loop {i}")
+        coords = coords[0]
+        output_t_jakob, _, _ = jakob_shred.forward(X, coords)
+        output_t_SD = SD_shred.forward(X)
+        plt.figure(figsize=(10,6))
+        plt.plot(coords.cpu().numpy(), output_t_SD[0].detach().cpu().numpy(), label='SHRED', linewidth=2.5)
+        plt.plot(coords.cpu().numpy(), output_t_jakob[0].detach().cpu().numpy(), label='Modified SHRED', linewidth=2.5)
+        plt.plot(coords.cpu().numpy(), y[0].cpu().numpy(), '--', label='True', linewidth=2)
+        if training_skips >= 10:
+                plt.plot(coords[::training_skips,:] .cpu().numpy(), y[0,::training_skips].cpu().numpy(), 'o', color = "black",label='Training data used', markersize=2)
+        # plt.plot(coords[240:250,:] .cpu().numpy(), y[0,240:250].cpu().numpy(), 'o', color = "black",label='Training data used', markersize=2)
+        plt.title(f'SHRED Predictions on test data at t = {t[len(train_dataset)+i]:.4f} \n'
+                  f"Using {nr_sensors} sensors, lags = {lags}, Nx = {N} and Nt = {nr_t_steps}\n" 
+                  f"Modified model uses {(N//training_skips)/N * 100}% of points in x")
+        plt.xlabel('x')
+        plt.ylabel('u(x,t)')
+        first_sensor = True
+        for sensor in sensors:
+            if first_sensor:
+                plt.plot(coords[sensor].cpu().numpy(), y[0,sensor].cpu().numpy(),"o", color='red', label='Sensors',markersize=5)
+                first_sensor = False
+            else:
+                plt.plot(coords[sensor].cpu().numpy(), y[0,sensor].cpu().numpy(),"o", color='red', markersize=5)
+        plt.legend()
+        plt.grid()
+        plt.xlim([-L/2, L/2])
+        plt.ylim([-1.2, 1.2])
+        buf = BytesIO()
+        plt.savefig(buf, format = 'png')
+        buf.seek(0)
+        images_test.append(Image.open(buf))
+        plt.close()
+        plt.show()
+
+    save_path = folder_path + f"Test_s{nr_sensors}_xskips{training_skips}_endtime{t_end:.2f}.gif"
+
+    images_test[0].save(save_path,
+                save_all=True,
+                append_images=images_test[1:],
+                duration=200,
+                loop=0)
+
+print("BING BONG!")
